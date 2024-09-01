807498c2a6b7681facb29ab39485d1d917749605
AjithIndika <kindika144@mail.com>
2024-09-01 01:59:25 -0700

    read me file

--- resources/views/users/ticket-user-rejester.blade.php
+++ resources/views/users/ticket-user-rejester.blade.php
@@ -37,9 +37,9 @@
       <div class="mask" style="background-color: rgba(0, 0, 0, 0.678);">
         <div class="d-flex justify-content-center align-items-center h-100">
           <div class="text-white">
-            <h1 class="mb-3">Asset Networks</h1>
+            <h1 class="mb-3">{{ config('app.name') }}</h1>
             <h4 class="mb-3">Support Ticket System</h4>
-            <a data-mdb-ripple-init class="btn btn-outline-light btn-lg" href="htps://hr.assetnetworks.net" role="button"
+            <a data-mdb-ripple-init class="btn btn-outline-light btn-lg" href="{{ config('app.url') }}" role="button"
             >Call to action</a
             >
           </div>
